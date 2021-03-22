13dae5d3662e371e09c8795a4830038256d53ae4
Azadeh <azadeh.ghahremani@unibg.it>
2021-03-22 16:11:10 +0100

    First upload
    Base files uploaded

---
+++ Melody/main.c
@@ -0,0 +1,21 @@
+/*
+ * Melody.c
+ *
+ * 
+ * Author : FaraDars
+ */ 
+
+#include <avr/io.h>
+#include "sound.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		beep(200,80);
+		beep(100,160);
+    }
+}
+
